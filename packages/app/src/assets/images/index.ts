149afcd2a719345e501d8e5f12baaf2836fa1d25
Paulo02-pixel <dev01@cociname.pe>
2026-01-02 17:13:37 -0500

    Fix: Arreglo de Logo para Web

--- packages/app/src/assets/images/index.ts
+++ packages/app/src/assets/images/index.ts
@@ -1,8 +1,8 @@
 import { Platform } from 'react-native';
 
 export const images = {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   logo: Platform.OS === 'web' 
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     ? require('./logo.png').default || require('./logo.png')
     : require('./logo.png')
 };
