c7ff6478bdeb04d8fc50775cabb1a40ca4ab7ee9
jonathan.seminario <jonathan.seminario@hotmail.com>
2026-06-24 16:41:14 -0500

    update config.ts to dev endpoints

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: "/api/",
-  HUB_URL: "/notificationHub",
+  BASE_URL: "https://apidev.cociname.pe/api/",
+  HUB_URL: "https://apidev.cociname.pe/notificationHub",
   TIMEOUT: 30000, // 30 segundos
 };
 
